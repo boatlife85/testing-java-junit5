ff184185d81c16042e1b1848f886d25570738a97
Joshua S <35569800+boatlife85@users.noreply.github.com>
2020-05-13 14:03:08 -0400

    Imported assertJ into POM file (adding the maven dependencies limited to the test scope).  Added a single assertJ method to demonstrate methods available and usages.

--- src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.condition.*;
 
 import java.time.Duration;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
@@ -23,12 +24,15 @@ class IndexControllerTest {
     @DisplayName("Test Proper View name is returned for index page")
     @Test
     void index() {
+        //JUnit Assertions
         assertEquals("index", controller.index());
         assertEquals("index", controller.index(), "Wrong View Returned");
 
         assertEquals("index", controller.index(), () -> "Another Expensive Message " +
                 "Make me only if you have to");
 
+        //assertJ assertion
+        assertThat(controller.index()).isEqualTo("index");
     }
 
     @Test
